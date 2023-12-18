b1c9aafd150b91e7173817c6c2fcabf782ccca8f
mathlon26 <92529978+mathlon26@users.noreply.github.com>
2023-12-18 16:32:00 +0100

    Update

--- web/static/js/bot.js
+++ web/static/js/bot.js
@@ -1,35 +1,32 @@
+// Global variable to store the currently active timeframe
+let activeTimeframe = 'monthly';
+
 // Function to start the bot
 async function startBot() {
     let goodToGo = await eel.start_bot()();
-    console.log(goodToGo);
 
     if (goodToGo){
-    document.getElementById('activeStatusText').innerText = 'Bot Active';
-    document.getElementById('activeStatus').classList.remove('bg-danger');
-    document.getElementById('activeStatus').classList.add('bg-success');
-    document.getElementById('startBtn').disabled = true;
-    document.getElementById('stopBtn').disabled = false;
-    }
-    else
-    {
-        msgBox("Bot is running, therefor you cannot start him.", "text-danger");
+        document.getElementById('activeStatusText').innerText = 'Bot Active';
+        document.getElementById('activeStatus').classList.remove('bg-danger');
+        document.getElementById('activeStatus').classList.add('bg-success');
+        document.getElementById('startBtn').disabled = true;
+        document.getElementById('stopBtn').disabled = false;
+    } else {
+        msgBox("Bot is running, therefore you cannot start him.", "text-danger");
     }
 }
 
 // Function to stop the bot
 async function stopBot() {
     let goodToGo = await eel.stop_bot()();
-    console.log(goodToGo);
     if (goodToGo){
         document.getElementById('activeStatusText').innerText = 'Bot Inactive';
         document.getElementById('activeStatus').classList.remove('bg-success');
         document.getElementById('activeStatus').classList.add('bg-danger');
         document.getElementById('startBtn').disabled = false;
         document.getElementById('stopBtn').disabled = true;
-    }
-    else
-    {
-        msgBox("Bot is not running, therefor you cannot stop him.", "text-danger");
+    } else {
+        msgBox("Bot is not running, therefore you cannot stop him.", "text-danger");
     }
 }
 
@@ -38,8 +35,8 @@ async function getPositions() {
     return positions;
 }
 
-async function getHistory() {
-    let history = await eel.get_history(30)();
+async function getHistory(time) {
+    let history = await eel.get_history(time)();
     return history;
 }
 
@@ -52,9 +49,6 @@ async function setOpenPositionsTable() {
     // Iterate through positions and create rows
     positions.forEach(position => {
         const row = document.createElement("tr");
-        console.log('+------+');
-        console.log(position);
-        console.log('+------+');
         // Create cells and populate with data
         const cells = ["id", "open_time", "symbol", "lotsize", "type", "open_price", "current_price", "profit", "action"];
         cells.forEach(cell => {
@@ -71,10 +65,13 @@ async function setOpenPositionsTable() {
                 closeButton.classList.add("btn", "btn-sm", "btn-danger");
                 
                 closeButton.onclick = async () => {
-                    
-
                     let result = await eel.close_position(position["position_id"], position["symbol"], position["lotsize"])();
-                    console.log(result);
+                    if (result[7] != "") {
+                        msgBox(result[7]);
+                    }
+                    else {
+                        msgBox(result);
+                    }
                     setOpenPositionsTable();
                 };
                 cellElement.appendChild(closeButton);
@@ -90,41 +87,89 @@ async function setOpenPositionsTable() {
     });
 }
 
-setOpenPositionsTable()
-setInterval(setOpenPositionsTable, 1500);
+setOpenPositionsTable();
+setInterval(setOpenPositionsTable, 6000);
 
+// Helper function to get history and set the table
+async function getHistoryAndSetTable(time) {
+    await setHistoryTable(time);
+}
 
-async function setHistoryTable() {
+// Set the initial timeframe and table on page load
+document.addEventListener('DOMContentLoaded', function () {
+    getHistoryAndSetTable(30);  // Set default to monthly (you can change this to your desired default)
+});
+
+function toggleTimeframe(timeframe) {
+    let time = 0;
+    switch (timeframe) {
+        case 'daily':
+            time = 1;
+            break;
+        case 'monthly':
+            time = 30;
+            break;
+        case 'yearly':
+            time = 365;
+            break;
+        // Add more cases if needed
+    }
+    
+    // Update the active timeframe
+    activeTimeframe = timeframe;
+
+    // Call the getHistory function with the selected timeframe
+    getHistoryAndSetTable(time);
+
+    // Update the button styles based on the active timeframe
+    updateButtonStyles(time);
+}
+
+async function setHistoryTable(time) {
     const tableBody = document.getElementById("historyTableBody");
-    let history = await getHistory();
+    let history = await getHistory(time);
     // Clear existing rows
     tableBody.innerHTML = "";
-    console.log(history);
     // Iterate through positions and create rows
     history.forEach(position => {
-        const row = document.createElement("tr");
 
         // Create cells and populate with data
         const cells = ["id", "open_time", "symbol", "lotsize", "type", "open_price", "current_price", "profit"];
-        cells.forEach(cell => {
-            const cellElement = document.createElement("td");
+        if (position["profit"] != 0)
+        {
+            const row = document.createElement("tr");
 
-            // Special handling for the "profit" cell to set text color
-            if (cell === "profit") {
-                cellElement.textContent = position[cell];
-                cellElement.classList.add(position[cell] < 0 ? "text-danger" : "text-success");
-                cellElement.textContent = position[cell];
-            }else {
-                cellElement.textContent = position[cell];
-            }
+            cells.forEach(cell => {
+                const cellElement = document.createElement("td");
 
-            row.appendChild(cellElement);
-        });
+                // Special handling for the "profit" cell to set text color
+                if (cell === "profit") {
+                    cellElement.textContent = position[cell];
+                    cellElement.classList.add(position[cell] < 0 ? "text-danger" : "text-success");
+                } else {
+                    cellElement.textContent = position[cell];
+                }
 
-        // Append the row to the table body
-        tableBody.appendChild(row);
+                row.appendChild(cellElement);
+            });
+
+            // Append the row to the table body
+            tableBody.appendChild(row);
+        }
     });
 }
 
-//setHistoryTable()
-//setInterval(setHistoryTable, 1500);
+// Helper function to update button styles based on the active timeframe
+function updateButtonStyles(time) {
+    // Remove the 'active' class from all buttons
+    const buttons = document.querySelectorAll('.toggleTimeFrameC');
+    buttons.forEach(button => {
+        button.classList.remove('active');
+    });
+
+    // Add the 'active' class to the button corresponding to the active timeframe
+    const activeButton = document.querySelector(`.toggleTimeFrameC[data-timeframe="${activeTimeframe}"]`);
+    if (activeButton) {
+        activeButton.classList.add('active');
+    }
+}
